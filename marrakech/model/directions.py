bbfaf5f1e11a464c7c6c15b3b5613d7d8560971c
Olivér Ősz <16599031+oliverosz@users.noreply.github.com>
2023-11-04 13:03:09 +0100

    Add Direction.rotate

--- marrakech/model/directions.py
+++ marrakech/model/directions.py
@@ -1,14 +1,18 @@
 from enum import Enum
 
 
+class RotationDirection(Enum):
+    LEFT = -1
+    NONE = 0
+    RIGHT = 1
+
+
 class Direction(Enum):
     UP = 0
     RIGHT = 1
     DOWN = 2
     LEFT = 3
 
-
-class RotationDirection(Enum):
-    NONE = 0
-    LEFT = 1
-    RIGHT = 2
+    def rotate(self, direction: "RotationDirection") -> "Direction":
+        """Visszaadja a megadott irányba elforgatott irányt"""
+        return Direction((self.value + direction.value) % 4)
